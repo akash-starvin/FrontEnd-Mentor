eaf991afa30e4651306b0a633d048b88edfe2502
akash-starvin <Akashstarvin999@gmail.com>
2021-11-25 13:59:17 +0530

    Implemented Header Component

--- src/app/crowdfunding-product-page/crowdfunding-product-page.module.ts
+++ src/app/crowdfunding-product-page/crowdfunding-product-page.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CrowdfundingProductPageRoutingModule } from './crowdfunding-product-page-routing.module';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    HeaderComponent
+  ],
   imports: [
     CommonModule,
     CrowdfundingProductPageRoutingModule
