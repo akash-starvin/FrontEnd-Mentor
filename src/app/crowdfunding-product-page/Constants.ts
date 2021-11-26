07413dfb9c5548516b14fbf8b42bfa58030d4213
akash-starvin <Akashstarvin999@gmail.com>
2021-11-26 19:47:31 +0530

    Simplified the code

---
+++ src/app/crowdfunding-product-page/Constants.ts
@@ -0,0 +1,36 @@
+export class Constants{
+  public static pledgeCardsData:any = [
+    {
+      title: 'Pledge with no reward',
+      amount: 0,
+      description:
+        'Choose to support us without a reward if you simply believe in our project. As a backer, you will be signed up to receive product updates via email.',
+      stock: 0,
+      key: 'no-reward'
+    },
+    {
+      title: 'Bamboo Stand',
+      amount: 'Pledge $25 or more',
+      description:
+        "You get an ergonomic stand made of natural bamboo. You've helped us launch our promotional campaign, and you’ll be added to a special Backer member list.",
+      stock: 101,
+      key: 'bamboo-stand'
+    },
+    {
+      title: 'Black Edition Stand',
+      amount: 'Pledge $75 or more',
+      description:
+        'You get a Black Special Edition computer stand and a personal thank you. You’ll be added to our Backer member list. Shipping is included.',
+      stock: 64,
+      key: 'black-edition-stand'
+    },
+    {
+      title: 'Mahogany Special Edition',
+      amount: 'Pledge $200 or more',
+      description:
+        'You get two Special Edition Mahogany stands, a Backer T-Shirt, and a personal thank you. You’ll be added to our Backer member list. Shipping is included.',
+      stock: 0,
+      key: 'mahogany-special-edition'
+    }
+  ];
+}
--- src/app/crowdfunding-product-page/about/about.component.ts
+++ src/app/crowdfunding-product-page/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Constants } from '../Constants';
 
 @Component({
   selector: 'about',
@@ -6,30 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent implements OnInit {
-  pledgeCardsData = [
-    {
-      title: 'Bamboo Stand',
-      amount: 'Pledge $25 or more',
-      description:
-        "You get an ergonomic stand made of natural bamboo. You've helped us launch our promotional campaign, and you’ll be added to a special Backer member list.",
-      stock: 101,
-    },
-    {
-      title: 'Black Edition Stand',
-      amount: 'Pledge $75 or more',
-      description:
-        'You get a Black Special Edition computer stand and a personal thank you. You’ll be added to our Backer member list. Shipping is included.',
-      stock: 64,
-    },
-    {
-      title: 'Mahogany Special Edition',
-      amount: 'Pledge $200 or more',
-      description:
-        'You get two Special Edition Mahogany stands, a Backer T-Shirt, and a personal thank you. You’ll be added to our Backer member list. Shipping is included.',
-      stock: 0,
-    },
-  ];
+  pledgeCardsData = Constants.pledgeCardsData.slice();
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.pledgeCardsData.shift();
+  }
 }
