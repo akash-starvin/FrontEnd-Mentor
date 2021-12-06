e0028cd2ed2ad07f2af34a854c2bc0b33ca8fcac
akash-starvin <Akashstarvin999@gmail.com>
2021-12-06 13:08:08 +0530

    Designed Home Component

--- src/app/social-media-dashboard/components/home/home.component.ts
+++ src/app/social-media-dashboard/components/home/home.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  darkMode: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  changeStatus() {
+    this.darkMode = ! this.darkMode;
   }
-
 }
