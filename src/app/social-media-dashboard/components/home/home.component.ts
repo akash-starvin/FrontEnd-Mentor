a92fac48b049bb52ea0b591502718845ccc20002
akash-starvin <Akashstarvin999@gmail.com>
2021-12-06 18:40:34 +0530

    Added mock data for Overview Cards

--- src/app/social-media-dashboard/components/home/home.component.ts
+++ src/app/social-media-dashboard/components/home/home.component.ts
@@ -15,7 +15,7 @@ export class HomeComponent implements OnInit {
       stats: '12',
       statsTrend: 'up',
       handleName: 'Facebook',
-      logo: '../../../../assets/social-media-dashboard/icons/icon-facebook.svg'
+      logo: '../../../../assets/social-media-dashboard/icons/icon-facebook.svg',
     },
     {
       userName: '@nathanf',
@@ -23,7 +23,7 @@ export class HomeComponent implements OnInit {
       stats: '99',
       statsTrend: 'up',
       handleName: 'Twitter',
-      logo: '../../../../assets/social-media-dashboard/icons/icon-twitter.svg'
+      logo: '../../../../assets/social-media-dashboard/icons/icon-twitter.svg',
     },
     {
       userName: '@realnathanf',
@@ -31,17 +31,84 @@ export class HomeComponent implements OnInit {
       stats: '1099',
       statsTrend: 'up',
       handleName: 'Instagram',
-      logo: '../../../../assets/social-media-dashboard/icons/icon-instagram.svg'
-    },{
+      logo: '../../../../assets/social-media-dashboard/icons/icon-instagram.svg',
+    },
+    {
       userName: 'Nathan F',
       followers: '8239',
       stats: '144',
       statsTrend: 'down',
       handleName: 'Youtube',
-      logo: '../../../../assets/social-media-dashboard/icons/icon-youtube.svg'
-    }
+      logo: '../../../../assets/social-media-dashboard/icons/icon-youtube.svg',
+    },
   ];
 
+  overviewCardData = [
+    {
+      title: 'Page View',
+      handleName: 'Facebook',
+      count: '87',
+      stats: 3,
+      statsTrend: 'up',
+      logo: '../../../../assets/social-media-dashboard/icons/icon-facebook.svg',
+    },
+    {
+      title: 'Likes',
+      handleName: 'Facebook',
+      count: '52',
+      stats: 2,
+      statsTrend: 'down',
+      logo: '../../../../assets/social-media-dashboard/icons/icon-facebook.svg',
+    },
+    {
+      title: 'Likes',
+      handleName: 'Instagram',
+      count: '5462',
+      stats: 2257,
+      statsTrend: 'up',
+      logo: '../../../../assets/social-media-dashboard/icons/icon-instagram.svg',
+    },
+    {
+      title: 'Profile View',
+      handleName: 'Instagram',
+      count: '52k',
+      stats: 1375,
+      statsTrend: 'up',
+      logo: '../../../../assets/social-media-dashboard/icons/icon-instagram.svg',
+    },
+    {
+      title: 'Retweets',
+      handleName: 'Twitter',
+      count: '117',
+      stats: 303,
+      statsTrend: 'up',
+      logo: '../../../../assets/social-media-dashboard/icons/icon-twitter.svg',
+    },
+    {
+      title: 'Likes',
+      handleName: 'Twitter',
+      count: '507',
+      stats: 553,
+      statsTrend: 'up',
+      logo: '../../../../assets/social-media-dashboard/icons/icon-twitter.svg',
+    },
+    {
+      title: 'Likes',
+      handleName: 'Youtube',
+      count: '107',
+      stats: 19,
+      statsTrend: 'down',
+      logo: '../../../../assets/social-media-dashboard/icons/icon-youtube.svg',
+    },
+    {
+      title: 'Total Views',
+      handleName: 'Youtube',
+      count: '1407',
+      stats: 12,
+      statsTrend: 'down',
+      logo: '../../../../assets/social-media-dashboard/icons/icon-youtube.svg',
+    },
+  ];
   constructor() {}
 
   ngOnInit(): void {}
