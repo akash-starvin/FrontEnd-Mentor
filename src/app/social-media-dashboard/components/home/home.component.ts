d96d595f99c332a6469a010dd6fb338630b267b7
akash-starvin <Akashstarvin999@gmail.com>
2021-12-06 13:20:26 +0530

    Added mock data and assets

--- src/app/social-media-dashboard/components/home/home.component.ts
+++ src/app/social-media-dashboard/components/home/home.component.ts
@@ -8,11 +8,45 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   darkMode: boolean = false;
 
+  socialMediaData = [
+    {
+      userName: '@nathanf',
+      followers: '1987',
+      stats: '12',
+      statsTrend: 'up',
+      handleName: 'Facebook',
+      logo: '../../../../assets/social-media-dashboard/icons/icon-facebook.svg'
+    },
+    {
+      userName: '@nathanf',
+      followers: '1044',
+      stats: '99',
+      statsTrend: 'up',
+      handleName: 'Twitter',
+      logo: '../../../../assets/social-media-dashboard/icons/icon-twitter.svg'
+    },
+    {
+      userName: '@realnathanf',
+      followers: '11k',
+      stats: '1099',
+      statsTrend: 'up',
+      handleName: 'Instagram',
+      logo: '../../../../assets/social-media-dashboard/icons/icon-instagram.svg'
+    },{
+      userName: 'Nathan F',
+      followers: '8239',
+      stats: '144',
+      statsTrend: 'down',
+      handleName: 'Youtube',
+      logo: '../../../../assets/social-media-dashboard/icons/icon-youtube.svg'
+    }
+  ];
+
   constructor() {}
 
   ngOnInit(): void {}
 
   changeStatus() {
-    this.darkMode = ! this.darkMode;
+    this.darkMode = !this.darkMode;
   }
 }
