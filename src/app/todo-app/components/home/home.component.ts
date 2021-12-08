c9edfcba17b094e52e8b1ecefcd8498c0bbac122
akash-starvin <Akashstarvin999@gmail.com>
2021-12-08 16:08:33 +0530

    Todo app: Designed filters

--- src/app/todo-app/components/home/home.component.ts
+++ src/app/todo-app/components/home/home.component.ts
@@ -3,40 +3,55 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
   isDarkMode: boolean = false;
   userInput: string = '';
+  selectedFilter: string = 'all';
 
   todoList = [
     {
       active: false,
-      task: 'Complete online JavaScript course'
+      task: 'Complete online JavaScript course',
     },
     {
       active: true,
-      task: 'Jog arround the park 3x'
+      task: 'Jog arround the park 3x',
     },
     {
       active: true,
-      task: '10 minutes meditation'
+      task: '10 minutes meditation',
     },
     {
       active: true,
-      task: 'Read for 1 hour'
+      task: 'Read for 1 hour',
     },
     {
       active: true,
-      task: 'Pick up groceries'
+      task: 'Pick up groceries',
     },
     {
       active: true,
-      task: 'Complete todo app'
-    }
+      task: 'Complete todo app',
+    },
   ];
 
-  constructor() { }
+  filterArray = [
+    {
+      key: 'all',
+      displayName: 'All',
+    },
+    {
+      key: 'active',
+      displayName: 'Active',
+    },
+    {
+      key: 'completed',
+      displayName: 'Completed',
+    },
+  ];
+  constructor() {}
 
   ngOnInit(): void {}
 
@@ -50,5 +65,15 @@ export class HomeComponent implements OnInit {
       task: this.userInput,
     });
     this.userInput = '';
+  }
+
+  applyFilter(key: string) {
+    if (key != this.selectedFilter) {
+      this.selectedFilter = key;
+    }
+  }
+
+  getRemainingTaskCount() {
+    return this.todoList.filter((item) => item.active).length;
   }
 }
