caefff94157af1012abcd7bc776854046d675c16
akash-starvin <Akashstarvin999@gmail.com>
2021-12-08 12:51:44 +0530

    Todo app: Added JSON mock data

--- src/app/todo-app/components/home/home.component.ts
+++ src/app/todo-app/components/home/home.component.ts
@@ -10,6 +10,33 @@ export class HomeComponent implements OnInit {
   isDarkMode: boolean = false;
   userInput: string = '';
 
+  todoList = [
+    {
+      active: false,
+      task: 'Complete online JavaScript course'
+    },
+    {
+      active: true,
+      task: 'Jog arround the park 3x'
+    },
+    {
+      active: true,
+      task: '10 minutes meditation'
+    },
+    {
+      active: true,
+      task: 'Read for 1 hour'
+    },
+    {
+      active: true,
+      task: 'Pick up groceries'
+    },
+    {
+      active: true,
+      task: 'Complete todo app'
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
