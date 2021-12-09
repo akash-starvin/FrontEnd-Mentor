bde3c563ece3f81ac6d9355383a5a8ac1c89f7cc
akash-starvin <Akashstarvin999@gmail.com>
2021-12-09 19:55:09 +0530

    Job Listing: Added Job Card component

--- src/app/job-listing/components/home/home.component.ts
+++ src/app/job-listing/components/home/home.component.ts
@@ -1,18 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FetchApiService } from '../../services/fetch-api.service';
+import { Job } from '../interface/job';
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  URL: string = './../../../assets/job-listing/mock-data/job-list.json';
+  jsonResponse: Job[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fetchApi: FetchApiService) {
+    this.getMockData(this.URL);
   }
 
-  clearAll(){
+  ngOnInit(): void {}
 
+  clearAll() {}
+
+  getMockData(url: string){
+    this.fetchApi.getJSON(url).subscribe(data => {
+      this.jsonResponse = data;
+  });
   }
 }
--- src/app/job-listing/job-listing.module.ts
+++ src/app/job-listing/job-listing.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { JobListingRoutingModule } from './job-listing-routing.module';
 import { HomeComponent } from './components/home/home.component';
+import { JobCardComponent } from './components/job-card/job-card.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    JobCardComponent
   ],
   imports: [
     CommonModule,
