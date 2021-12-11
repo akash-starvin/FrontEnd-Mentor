602e66d0d5e35819fdbece764c463b30af05c39d
akash-starvin <Akashstarvin999@gmail.com>
2021-12-11 21:45:44 +0530

    IP Tracker: Implemented Map and fetch IP address

--- src/app/ip-address-tracker/component/tracker-home/tracker-home.component.ts
+++ src/app/ip-address-tracker/component/tracker-home/tracker-home.component.ts
@@ -1,18 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { MapService } from '../../service/map.service';
 
 @Component({
   selector: 'app-tracker-home',
   templateUrl: './tracker-home.component.html',
   styleUrls: ['./tracker-home.component.scss']
 })
-export class TrackerHomeComponent implements OnInit {
+export class TrackerHomeComponent implements OnInit, AfterViewInit {
+  KEY: string = 'at_sy9223SRvnRqB06xiV3uU9Zmlb3po';
+  userInputIP: string = '8.8.8.8';
+  map: any;
 
-  userInput: string = '';
+  constructor(private mapServicce: MapService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.searchIPAddress();
   }
 
-  findIPAddress(){}
+  searchIPAddress() {
+    fetch(
+      `https://geo.ipify.org/api/v1?apiKey=${this.KEY}&ipAddress=${this.userInputIP}`
+    )
+      .then((result) => result.json())
+      .then((data) => {
+        console.log(data); // remove log
+        this.resetMap();
+        this.map = this.mapServicce.initMap(
+          data.location.lat,
+          data.location.lng,
+          'map-container'
+        );
+      });
+  }
+
+  resetMap(){
+    if (this.map !== undefined && this.map !== null) {
+      this.map.remove();
+    }
+  }
 }
---
+++ src/app/ip-address-tracker/service/map.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import * as L from 'leaflet';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MapService {
+  MARK_ICON = L.icon({
+    iconUrl: '../../../../assets/ip-address-tracker/icons/icon-location.svg',
+    iconSize: [40, 40],
+  });
+
+  TILE_LAYER = L.tileLayer(
+    'https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png',
+    {
+      maxZoom: 20,
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }
+  );
+
+  constructor() {}
+
+  initMap(lat: number, lng: number, viewID: string) {
+    let map;
+    map = L.map(viewID).setView([lat, lng], 15);
+    this.TILE_LAYER.addTo(map);
+    L.marker([lat, lng], {
+      icon: this.MARK_ICON,
+    }).addTo(map);
+    return map;
+  }
+}
