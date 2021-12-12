087e438a9138ea28f89d5b2fc5f69ca1293ec70d
akash-starvin <Akashstarvin999@gmail.com>
2021-12-12 14:36:47 +0530

    IP Tracker: Implemented card info

--- src/app/ip-address-tracker/component/tracker-home/tracker-home.component.ts
+++ src/app/ip-address-tracker/component/tracker-home/tracker-home.component.ts
@@ -10,6 +10,10 @@ export class TrackerHomeComponent implements OnInit, AfterViewInit {
   KEY: string = 'at_sy9223SRvnRqB06xiV3uU9Zmlb3po';
   userInputIP: string = '8.8.8.8';
   map: any;
+  location: string = '-';
+  timeZone: string = '-';
+  isp: string = '-';
+  ipAddress: string = '-';
 
   constructor(private mapServicce: MapService) {}
 
@@ -25,25 +29,42 @@ export class TrackerHomeComponent implements OnInit, AfterViewInit {
     )
       .then((result) => result.json())
       .then((data) => {
-        if (data.code === 422) {
-          alert("Enter a valid IP Address");
-        } else {
-          this.resetMap();
-          this.map = this.mapServicce.initMap(
-            data.location.lat,
-            data.location.lng,
-            'map-container'
-          );
-        }
+        this.setMap(data);
       })
       .catch((error) => {
         console.error('There was an error!', error);
       });
   }
 
+  setMap(data: any) {
+    if (data.code === 422) {
+      alert('Enter a valid IP Address');
+    } else {
+      this.getCardData(data);
+      this.resetMap();
+      this.map = this.mapServicce.initMap(
+        data.location.lat,
+        data.location.lng,
+        'map-container'
+      );
+    }
+  }
+
   resetMap() {
     if (this.map !== undefined && this.map !== null) {
       this.map.remove();
     }
+  }
+
+  getCardData(data: any) {
+    this.location = this.getLoacation(data.location);
+    this.isp = data.isp;
+    this.timeZone = 'UTC ' + data.location.timezone;
+    this.ipAddress = data.ip;
+    console.log(this.location, this.isp, this.timeZone);
+  }
+
+  getLoacation(location: any): string {
+    return location.region + ', ' + location.region + ' ' + location.postalCode;
   }
 }
