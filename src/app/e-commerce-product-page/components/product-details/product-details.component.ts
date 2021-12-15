425c4a060701d6b2bd3568cb4dbce3252f6381c6
akash-starvin <Akashstarvin999@gmail.com>
2021-12-15 11:43:38 +0530

    E-commerce: Update product count

--- src/app/e-commerce-product-page/components/product-details/product-details.component.ts
+++ src/app/e-commerce-product-page/components/product-details/product-details.component.ts
@@ -8,14 +8,33 @@ import { Component, Input, OnInit } from '@angular/core';
 export class ProductDetailsComponent implements OnInit {
   @Input() details: any;
   actualPrice: any;
-
+  MAX_COUNT: number = 10;
+  count: number = 0;
   constructor() {}
 
   ngOnInit(): void {
-    this.actualPrice = this.calculateActualPrice(this.details.price, this.details.discount);
+    this.actualPrice = this.calculateActualPrice(
+      this.details.price,
+      this.details.discount
+    );
   }
 
   calculateActualPrice(price: number, discount: number) {
-    return (price * (discount / 100)).toFixed(2);;
+    return (price * (discount / 100)).toFixed(2);
+  }
+
+  updateCount(val: number) {
+    switch (val) {
+      case -1:
+        if (this.count > 0) this.count--;
+
+        break;
+      case 1:
+        if (this.count < this.MAX_COUNT) this.count++;
+
+        break;
+      case 0:
+        this.count = 0;
+    }
   }
 }
